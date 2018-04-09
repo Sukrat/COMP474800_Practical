22b53647faecc89ad5ca2fb6343a42c4e7a93851
Sukrat Kashyap <sukraat021195@gmail.com>
2018-04-09 17:16:00 +0100

    lab4: added folder

---
+++ lab4/SRP.java
@@ -0,0 +1,30 @@
+
+public class SRP {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
+
+class Hexapod {
+	public Hexapod(String name) {
+		_name = name;
+	}
+
+	public void throwStick() {
+		System.out.println(_name + "is throwing a stick");
+	}
+
+	public void fetchStick() {
+		System.out.println("I'm fetching a stick");
+	}
+
+	public void walk() {
+		System.out.println("I'm walking");
+	}
+
+	public void bark() {
+		System.out.println("Woof! ");
+	}
+
+	String _name;
+}
